a76e7158c1a654bebf510e338ef09011cf437562
Romain THERRAT <romain42@gmail.com>
2012-09-13 13:36:15 +0200

    FIX: Fix installation order for hba file
    In order to add data into hba file you have to install postgresql first.

---
+++ spec/defines/postgresql_hba_spec.rb
@@ -0,0 +1,18 @@
+require "#{File.join(File.dirname(__FILE__),'..','spec_helper.rb')}"
+
+describe 'postgresql::hba', :type => :define do
+
+  let(:title) { 'postgresql::hba' }
+  let(:node) { 'rspec.example42.com' }
+  let(:facts) { { :arch => 'i386', :operatingsystem => 'Debian' } }
+  let(:params) { {
+    :type     => 'local',
+    :database => 'all',
+    :user     => 'all',
+    :method   => 'password'
+  } }
+
+  describe 'Test postgresql::hba require postgresql to be install' do
+    it { should contain_concat('/etc/postgresql/8.4/main/pg_hba.conf').with_require(/Package/) }
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # Based on https://github.com/puppetlabs/puppetlabs-ntp/blob/master/spec/spec_helper.rb
-# Thanks to Ken Barber for advice about http://projects.puppetlabs.com/issues/11191  
+# Thanks to Ken Barber for advice about http://projects.puppetlabs.com/issues/11191
 require 'puppet'
 require 'rspec-puppet'
 require 'tmpdir'
@@ -18,7 +18,9 @@ RSpec.configure do |c|
   c.filter_run_excluding :broken => true
 
   c.after :each do
-    FileUtils.remove_entry_secure(@puppetdir)
+    if FileTest.exists?("#{@puppetdir}")
+      FileUtils.remove_entry_secure(@puppetdir)
+    end
   end
 
   c.module_path = File.join(File.dirname(__FILE__), '../../')
