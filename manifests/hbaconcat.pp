def401f6ebaa7a27f4a0b1385dbc872a6527d54a
David Schmitt <david@dasz.at>
2013-01-18 17:27:06 +0100

    Add hba_header template; Add forced local access for admin; typo

--- manifests/hbaconcat.pp
+++ manifests/hbaconcat.pp
@@ -11,15 +11,15 @@ class postgresql::hbaconcat {
   }
 
   # The File Header. With Puppet comment
-  concat::fragment { 'postgresqt_hba_header':
+  concat::fragment { 'postgresql_hba_header':
     target  => $postgresql::config_file_hba,
-    content => "# File Managed by Puppet\n",
+    content => template('postgresql/concat_hba_header.erb'),
     order   => '01',
     notify  => Service['postgresql'],
   }
 
   # The File Footer. With default acls
-  concat::fragment{ 'postgresqt_hba_footer':
+  concat::fragment { 'postgresql_hba_footer':
     target  => $postgresql::config_file_hba,
     content => template('postgresql/concat_hba_footer.erb'),
     order   => '90',
